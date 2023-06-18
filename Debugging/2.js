af89cd83c4a3e562300406c0fd5518e19d6127d1
Vinay001100 <102034089+Vinay001100@users.noreply.github.com>
2023-06-18 22:15:24 +0530

    Add files via upload

---
+++ Debugging/2.js
@@ -0,0 +1,12 @@
+let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+
+console.log(output);
+console.clear();
+
+
+/*
+Understanding the Differences between the freeCodeCamp and Browser Console
+You may have noticed that some freeCodeCamp challenges include their own console. This console behaves a little differently than the browser console.
+
+There are many methods to use with console to output messages. log, warn, and clear to name a few. The freeCodeCamp console will only output log messages, while the browser console will output all messages. When you make changes to your code, it will automatically run and show the logs. The freeCodeCamp console is then cleared each time your code runs.
+*/
