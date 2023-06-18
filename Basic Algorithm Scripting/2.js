a15deb4a56d65686089015cf4c90d08148889420
Vinay001100 <102034089+Vinay001100@users.noreply.github.com>
2023-06-18 22:14:08 +0530

    Add files via upload

---
+++ Basic Algorithm Scripting/2.js
@@ -0,0 +1,15 @@
+function reverseString(str) {
+    return str
+    .split("")
+      .reverse()
+      .join("");
+  }
+  
+  reverseString("hello");
+
+  /*
+  Reverse a String
+Reverse the provided string and return the reversed string.
+
+For example, "hello" should become "olleh".
+  */
---
+++ Basic Algorithm Scripting/4.js
@@ -0,0 +1,21 @@
+function findLongestWordLength(str) {
+  let words = str.split(' ');
+  let maxLength = 0;
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > maxLength) {
+      maxLength = words[i].length;
+    }
+  }
+
+  return maxLength;
+}
+
+/*
+Find the Longest Word in a String
+Return the length of the longest word in the provided sentence.
+
+Your response should be a number.
+
+
+*/
